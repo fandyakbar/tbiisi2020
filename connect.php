1f92e4478943b4e7467ec338fd74593af722dd31
fandyakbar <48479320+fandyakbar@users.noreply.github.com>
2020-11-17 12:52:13 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "fandy1113";
+	$host = "ec2-3-211-176-230.compute-1.amazonaws.com";
+	$user = "qpzadrcqaxaxjy";
+	$pass = "43b9366fc26c2cdde6e213b500325aa8db0d7acf4cdc1bcbb616abb6a2897e81";
 	$port = "5432";
-	$dbname = "bdl";
+	$dbname = "d7gagsg14f5tng";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
