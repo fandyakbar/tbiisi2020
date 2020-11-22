c7f7833ab3b0014e949ddbd4a95ea1ad9b9786ed
fandyakbar <48479320+fandyakbar@users.noreply.github.com>
2020-11-22 23:10:05 +0700

    update server

--- config_public.js
+++ config_public.js
@@ -3,5 +3,5 @@ var ipServerHotel="http://gissurya.org/hotel_bkt/";
 var ipServerTourism="http://gissurya.org/tourism_bkt/";*/
 
 var ipServerAngkot="http://localhost/html/angkot_bkt/";
-var ipServerHotel="http://localhost/kelompok6iisi/hotel_bkt/";
+var ipServerHotel="https://tbiisi2020fandy.herokuapp.com/hotel_bkt/";
 var ipServerTourism="http://localhost/html/tourism_bkt/";
